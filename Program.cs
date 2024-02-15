28afaea3bb9d87c1cc872440d5e11cd070fc6b72
cemile10 <c3van0ri@gmail.com>
2024-02-15 19:17:43 +0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿//3cu tapsirig
+Console.WriteLine("ucreqemli eded daxil edin");
+int eded = Convert.ToInt32(Console.ReadLine());
+
+int eded1 = eded / 100;
+int eded2 = (eded - (eded1 * 100)) / 10;
+int eded3 = (eded - ((eded1 * 100) + (eded2 * 10)));
+
+Console.WriteLine("ortadaki reqemi cixmis hali - " + (eded1) + eded3);
+
